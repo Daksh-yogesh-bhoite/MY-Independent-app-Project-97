78718e4e712ee8c2204f421d1d5a249efa6a3df6
Daksh-yogesh-bhoite <95295074+Daksh-yogesh-bhoite@users.noreply.github.com>
2022-05-25 16:26:19 +0530

    Add files via upload

---
+++ topic.js
@@ -0,0 +1,22 @@
+    function redirect_to_nature_screen(){
+    locate_nature_screen=document.getElementById("button_nature");
+    button_nature="Information about Nature"
+    window.location="nature.html";
+}
+
+function redirect_to_main_screen(){
+    locate_to_main_screen=document.getElementById("main_screen")
+    window.location="index.html"
+}
+
+function redirect_select_screen(){
+    window.location="select.html"
+}
+
+function redirect_to_boys_screen(){
+    window.location="boys.html"
+}
+
+function redirect_to_girls_screen(){
+    window.location="girls.html"
+}
